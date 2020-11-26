38f37ffc51f0c759e68968bc7df64d1f69c71acc
Woloda <dorozovecvoloda@gmail.com>
2020-11-26 23:16:26 +0200

    Lab__8_1_5

---
+++ Lab_8.1(5)/Lab_8.1(5).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char lit_r[20] = "ssqSQfqsSWfs";
+	cout << "Сфомований літерний рядок: " << endl << endl << lit_r << endl << endl;
+	char para[] = "SQ";
+	char para1[] = "QS";
+
+	bool b = false;
+	if (strstr(lit_r, para))
+		b = true;
+	if (strstr(lit_r, para1))
+		b = true;
+	if (b)
+		cout << "Так є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+	else
+		cout << "Ні не має серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+
+	return 0;
+}
---
+++ UnitTest(Lab_8.1(5))/UnitTest(Lab_8.1(5)).cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab815
+{
+	TEST_CLASS(UnitTestLab815)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char lit_r[20] = "ssqSQfqsSWfs";
+			char para[] = "SQ";
+			char para1[] = "QS";
+
+			bool b = false;
+			b = (strstr(lit_r, para));
+
+			Assert::AreEqual(b, true);
+		}
+	};
+}
